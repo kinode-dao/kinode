93397e32b4da94b13eac6c200e05402de96b3e1e
bitful-pannul <bitful-pancreas@protonmail.com>
2023-10-10 02:51:31 +0300

    wip script injection

--- modules/app_tracker/src/lib.rs
+++ modules/app_tracker/src/lib.rs
@@ -22,29 +22,32 @@ pub enum AppTrackerRequest {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct ManifestEntry {
-    id: String, // need to parse into ProcessId
-    path: String,
-    on_panic: kt::OnPanic,
-    networking: bool,
-    process_caps: Vec<String>,
+pub struct PackageManifestEntry {
+    pub process_name: String,
+    pub process_wasm_path: String,
+    pub on_panic: kt::OnPanic,
+    pub request_networking: bool,
+    pub request_messaging: Vec<String>,
+    pub grant_messaging: Vec<String>, // special logic for the string "all": makes process public
 }
 
-// TODO: error handle
 fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
     match serde_json::from_str(&request_string)? {
         AppTrackerRequest::New { package } => {
-            //  TODO: should we check if package already exists before creating?
+            print_to_terminal(0, "in app tracker");
 
             let Some(payload) = get_payload() else {
-                panic!("");
+                return Err(anyhow::anyhow!("no payload"));
             };
+            print_to_terminal(0, "after payload");
 
             let vfs_address = Address {
                 node: our.node.clone(),
                 process: ProcessId::from_str("vfs:sys:uqbar").unwrap(),
             };
             // make vfs package
+            print_to_terminal(0, "new vfs action");
+
             let _ = process_lib::send_and_await_response(
                 &vfs_address,
                 false,
@@ -59,6 +62,8 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                 None,
                 5,
             )?;
+
+            print_to_terminal(0, "we in here after new");
 
             // add zip bytes
             let _ = process_lib::send_and_await_response(
@@ -78,6 +83,8 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                 Some(&payload),
                 5,
             )?;
+            print_to_terminal(0, "we in here after zippie zip");
+
             Ok(())
         }
         AppTrackerRequest::Install { package } => {
@@ -85,7 +92,7 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                 node: our.node.clone(),
                 process: ProcessId::from_str("vfs:sys:uqbar").unwrap(),
             };
-            // get manifest
+
             let _ = process_lib::send_and_await_response(
                 &vfs_address,
                 false,
@@ -101,16 +108,16 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                 5,
             )?;
             let Some(payload) = get_payload() else {
-                panic!("");
+                return Err(anyhow::anyhow!("no payload"));
             };
             let manifest = String::from_utf8(payload.bytes)?;
-            let manifest = serde_json::from_str::<Vec<ManifestEntry>>(&manifest).unwrap();
+            let manifest = serde_json::from_str::<Vec<PackageManifestEntry>>(&manifest).unwrap();
 
             for entry in manifest {
-                let path = if entry.path.starts_with("/") {
-                    entry.path
+                let path = if entry.process_wasm_path.starts_with("/") {
+                    entry.process_wasm_path
                 } else {
-                    format!("/{}", entry.path)
+                    format!("/{}", entry.process_wasm_path)
                 };
 
                 let (_, hash_response) = process_lib::send_and_await_response(
@@ -128,15 +135,15 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                     5,
                 )?;
                 let Message::Response((Response { ipc: Some(ipc), .. }, _)) = hash_response else {
-                    panic!("baz");
+                    return Err(anyhow::anyhow!("bad vfs response"));
                 };
                 let kt::VfsResponse::GetHash(Some(hash)) = serde_json::from_str(&ipc).unwrap() else {
-                    panic!("aaa");
+                    return Err(anyhow::anyhow!("no hash in vfs"));
                 };
 
                 // build initial caps
                 let mut initial_capabilities: HashSet<kt::SignedCapability> = HashSet::new();
-                if entry.networking {
+                if entry.request_networking {
                     let Some(networking_cap) = get_capability(
                         &Address {
                             node: our.node.clone(),
@@ -144,7 +151,7 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                         },
                         &"\"network\"".to_string(),
                     ) else {
-                        panic!("app_tracker: no net cap");
+                        return Err(anyhow::anyhow!("app_tracker: no net cap"));
                     };
                     initial_capabilities.insert(kt::de_wit_signed_capability(networking_cap));
                 }
@@ -155,7 +162,7 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                         "drive": package,
                     })).unwrap(),
                 ) else {
-                    panic!("app_tracker: no read cap");
+                    return Err(anyhow::anyhow!("app_tracker: no read cap"));
                 };
                 initial_capabilities.insert(kt::de_wit_signed_capability(read_cap));
                 let Some(write_cap) = get_capability(
@@ -165,11 +172,12 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                         "drive": package,
                     })).unwrap(),
                 ) else {
-                    panic!("app_tracker: no write cap");
+                    return Err(anyhow::anyhow!("app_tracker: no write cap"));
                 };
                 initial_capabilities.insert(kt::de_wit_signed_capability(write_cap));
                 let mut public = false;
-                for process_name in entry.process_caps {
+
+                for process_name in &entry.grant_messaging {
                     if process_name == "all" {
                         public = true;
                         continue;
@@ -186,14 +194,47 @@ fn parse_command(our: &Address, request_string: String) -> anyhow::Result<()> {
                             "messaging": kt::ProcessId::de_wit(parsed_process_id),
                         })).unwrap(),
                     ) else {
-                        panic!("app_tracker: no cap");
+                        return Err(anyhow::anyhow!("app_tracker: no cap"));
                     };
                     initial_capabilities.insert(kt::de_wit_signed_capability(messaging_cap));
                 }
 
-                let Ok(parsed_new_process_id) = ProcessId::from_str(&entry.id) else {
-                    panic!("app_tracker: invalid process id");
+                // TODO fix request?
+                for process_name in &entry.request_messaging {
+                    let Ok(parsed_process_id) = ProcessId::from_str(process_name) else {
+                        continue;
+                    };
+                    let Some(messaging_cap) = get_capability(
+                        &Address {
+                            node: our.node.clone(),
+                            process: parsed_process_id.clone(),
+                        },
+                        &serde_json::to_string(&serde_json::json!({
+                            "messaging": kt::ProcessId::de_wit(parsed_process_id),
+                        })).unwrap(),
+                    ) else {
+                        return Err(anyhow::anyhow!("app_tracker: no cap"));
+                    };
+                    initial_capabilities.insert(kt::de_wit_signed_capability(messaging_cap));
+                }
+                let process_id = format!("{}:{}", entry.process_name, package.clone());
+                let Ok(parsed_new_process_id) = ProcessId::from_str(&process_id) else {
+                    return Err(anyhow::anyhow!("app_tracker: invalid process id!"));
                 };
+                let _ = process_lib::send_and_await_response(
+                    &Address {
+                        node: our.node.clone(),
+                        process: ProcessId::from_str("kernel:sys:uqbar").unwrap(),
+                    },
+                    false,
+                    Some(
+                        serde_json::to_string(
+                            &kt::KernelCommand::KillProcess(kt::ProcessId::de_wit(parsed_new_process_id.clone()))).unwrap()),
+                    None,
+                    None,
+                    5,
+                )?;
+
                 let _ = process_lib::send_and_await_response(
                     &Address {
                         node: our.node.clone(),
