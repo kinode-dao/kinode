72bb9a872ff131dd1538fb6d08f9ef6a5e179898
dr-frmr <docterformer@protonmail.com>
2023-11-22 16:31:32 -0500

    net: timeout on accept_async socket too

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -384,7 +384,7 @@ async fn direct_networking(
                 // to prevent some amount of potential DDoS attacks.
                 // can also block based on socket_addr
                 // ignore connections we failed to accept...?
-                if let Ok(websocket) = accept_async(MaybeTlsStream::Plain(stream)).await {
+                if let Ok(Ok(websocket)) = time::timeout(TIMEOUT, accept_async(MaybeTlsStream::Plain(stream))).await {
                     print_debug(&print_tx, "net: received new websocket connection").await;
                     let (peer_id, routing_for, conn) =
                         match time::timeout(TIMEOUT, recv_connection(
