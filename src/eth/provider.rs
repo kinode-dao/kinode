40e54eba272fec564f1ac3718b681fdccebd6eb3
commercium-sys <system@commercium.systems>
2023-12-25 21:51:50 -0500

    reorganized to not use closure but just bootstrap connection struct ahead of loop and pass in as borrowed

--- src/eth/provider.rs
+++ src/eth/provider.rs
@@ -1,11 +1,11 @@
-use crate::eth::types::EthRpcAction;
+use crate::eth::types::{ ProviderAction, EthRpcAction };
 use crate::http::types::HttpServerAction;
 use crate::types::*;
 use anyhow::Result;
 use ethers::core::types::Filter;
 use ethers::prelude::Provider;
 use ethers::types::{ValueOrArray, U256, U64};
-use ethers_providers::{Middleware, StreamExt, Ws};
+use ethers_providers::{Middleware, StreamExt, Ws, Http};
 use futures::SinkExt;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
@@ -13,8 +13,16 @@ use std::collections::HashMap;
 use std::future::Future;
 use std::pin::Pin;
 use tokio_tungstenite::connect_async;
-use tokio_tungstenite::WebSocketStream;
+use tokio_tungstenite::{MaybeTlsStream, WebSocketStream};
+use tokio::net::TcpStream;
 use url::Url;
+
+struct Connections {
+    ws_stream: Option<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+    ws_provider: Option<Provider<Ws>>,
+    http_provider: Option<Provider<Http>>,
+    uq_provider: Option<NodeId>
+}
 
 pub async fn provider(
     our: String,
@@ -53,13 +61,28 @@ pub async fn provider(
 
     send_to_loop.send(open_ws).await;
 
-    let dispatch = get_dispatch(rpc_url, send_to_loop.clone()).await;
+    let mut connections = Connections {
+        ws_stream: None,
+        ws_provider: None,
+        http_provider: None,
+        uq_provider: None
+    };
+
+    match Url::parse(&rpc_url).unwrap().scheme() {
+        "http" | "https" => { unreachable!() }
+        "ws" | "wss" => {
+            let (_ws_stream, _) = connect_async(&rpc_url).await.expect("failed to connect");
+            connections.ws_stream = Some(_ws_stream);
+            connections.ws_provider = Some(Provider::<Ws>::connect(rpc_url.clone()).await?);
+        }
+        _ => { unreachable!() }
+    }
 
     while let Some(km) = recv_in_client.recv().await {
         match km.message {
             Message::Request(Request { ref ipc, .. }) => {
                 println!("eth request");
-                handle_request(ipc)?;
+                handle_request(ipc, km.payload, &connections)?;
             }
             Message::Response((Response { ref ipc, .. }, ..)) => {
                 println!("eth response");
@@ -75,60 +98,16 @@ pub async fn provider(
     Ok(())
 }
 
-async fn get_dispatch(
-    rpc_url: String,
-    send_to_loop: MessageSender,
-) -> Box<dyn Fn(EthRpcAction) -> Pin<Box<dyn Future<Output = ()> + Send>>> {
-    let parsed = Url::parse(&rpc_url).unwrap();
+fn handle_request(ipc: &Vec<u8>, payload: Option<Payload>, connections: &Connections) -> Result<()> {
 
-    match parsed.scheme() {
-        "http" | "https" => {
-            unreachable!()
+    match serde_json::from_slice::<ProviderAction>(ipc)? {
+        ProviderAction::HttpServerAction(action) => {
+            println!("http server action {:?}", action);
         }
-        "ws" | "wss" => return ws_dispatch(rpc_url.clone(), send_to_loop).await,
-        _ => {
-            unreachable!()
+        ProviderAction::EthRpcAction(action) => {
+            println!("eth rpc action {:?}", action);
         }
     }
-}
-
-async fn ws_dispatch(
-    rpc_url: String,
-    send_to_loop: MessageSender,
-) -> Box<dyn Fn(EthRpcAction) -> Pin<Box<dyn Future<Output = ()> + Send>>> {
-    let provider = Provider::<Ws>::connect(&rpc_url).await;
-
-    Box::new(move |action| {
-        let send_to_loop = send_to_loop.clone();
-        let rpc_url = rpc_url.clone();
-        Box::pin(async move {
-            match action {
-                EthRpcAction::JsonRpcRequest(json) => {
-                    let (mut ws_stream, _) =
-                        connect_async(&rpc_url).await.expect("failed to connect");
-                    ws_stream
-                        .send(tokio_tungstenite::tungstenite::Message::Text(json))
-                        .await
-                        .unwrap();
-
-                    while let Some(msg) = ws_stream.next().await {}
-                }
-                EthRpcAction::Eth(method) => {}
-                EthRpcAction::Debug(method) => {}
-                EthRpcAction::Net(method) => {}
-                EthRpcAction::Trace(method) => {}
-                EthRpcAction::TxPool(method) => {}
-            };
-        })
-    })
-}
-
-fn handle_request(ipc: &Vec<u8>) -> Result<()> {
-    let Ok(message) = serde_json::from_slice::<HttpServerAction>(ipc) else {
-        return Ok(());
-    };
-
-    println!("request message {:?}", message);
 
     Ok(())
 }
