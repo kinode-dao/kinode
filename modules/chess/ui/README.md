6ab2d13a47a5c35d6a5ffeeea3cdf6000d9130de
Will Galebach <willbach@gmail.com>
2024-01-08 16:21:19 +0000

    Removed UI folder

--- modules/chess/ui/README.md
+++
@@ -1,3 +0,0 @@
-# chess-ui
-
-The UI for the Uqbar chess app. This UI is available at /chess:chess:uqbar/ on any Uqbar node.
--- modules/chess/ui/src/utils/url.ts
+++
@@ -1,5 +0,0 @@
-export const genFetchRoute = (route: string) => {
-  return window.location.pathname.includes('/http-proxy/serve/') ?
-    `/http-proxy/serve/${window.location.pathname.split('/')[3]}/${route}`:
-    route
-}
