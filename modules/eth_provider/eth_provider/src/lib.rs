718cbd3a266567256e3625800368cf155470579f
commercium-sys <system@commercium.systems>
2024-01-25 18:49:16 -0500

    added capability for eth_provider to app store, eth provider serializing subscribe logs request

--- modules/eth_provider/eth_provider/src/lib.rs
+++ modules/eth_provider/eth_provider/src/lib.rs
@@ -1,5 +1,6 @@
 use kinode_process_lib::eth_alloy::{
-    AlloySubscribeLogsRequest
+    EthProviderRequests,
+    RpcRequest
 };
 use kinode_process_lib::{
     Address,
@@ -15,8 +16,10 @@ use kinode_process_lib::http::{
     WsMessageType,
     HttpClientError,
     HttpClientResponse,
+    HttpServerRequest,
 };
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 #[derive(Debug, Serialize, Deserialize)]
 enum EthAction {
@@ -40,7 +43,8 @@ struct RpcPath {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct State {
-    conn: WsConnection
+    conn: WsConnection,
+    current_id: u64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -59,7 +63,7 @@ impl WsConnection {
     }
 
     fn send(&self, blob: Blob) {
-        http::send_ws_client_push(
+        let _ = http::send_ws_client_push(
             self.our.node.clone(),
             self.channel,
             WsMessageType::Text,
@@ -104,7 +108,7 @@ fn main(our: Address) -> anyhow::Result<()> {
     
     let mut state = match serde_json::from_slice::<Result<HttpClientResponse, HttpClientError>>(msg.body()) {
         Ok(Ok(HttpClientResponse::WebSocketAck)) => {
-            State { conn: WsConnection::new(rpc_path.process_addr, channel) }
+            State { conn: WsConnection::new(rpc_path.process_addr, channel), current_id: 0 }
         },
         _ => {
             return Err(anyhow::anyhow!(": failed to open ws connection"))
@@ -114,8 +118,11 @@ fn main(our: Address) -> anyhow::Result<()> {
     loop {
         match await_message() {
             Ok(msg) =>  {
-                handle_message(&our, msg, &mut state);
-                continue;
+                if msg.is_request() {
+                    let _ = handle_request(&our, msg, &mut state);
+                } else {
+                    let _ = handle_response(&our, msg, &mut state);
+                }
             }
             Err(e) => {
                 break;
@@ -129,20 +136,55 @@ fn main(our: Address) -> anyhow::Result<()> {
 }
 
 
-fn handle_message(our: &Address, msg: Message, state: &mut State) -> anyhow::Result<()> {
+fn handle_request(our: &Address, msg: Message, state: &mut State) -> anyhow::Result<()> {
 
-    match msg {
-
-        Message::Request { source, expects_response, body, metadata, capabilities } => { 
-
-            println!("~\n~\n~\n got request: {:?},{:?},{:?}", source, body, metadata);
-
+    // the body can have arbitrary data in it, and I want to detect if that data is a type of request. Do I need to match it against an enum?
+    // the body of the message is definitely a request, so I want to match whatever serialized data is in msg.body()
+    // what should I do here?
+    match serde_json::from_slice::<EthProviderRequests>(&msg.body()) { 
+        Ok(EthProviderRequests::RpcRequest(req)) => {
+            println!("~\n~\n~\n got request: {:?}", req);
+            let _ = handle_rpc_request(msg, req, state);
+            return Ok(());
         }
+        Err(e) => { }
+    }
 
-        Message::Response { source, body, metadata, context, capabilities } => { }
-
+    match serde_json::from_slice::<HttpServerRequest>(&msg.body()) {
+        Ok(HttpServerRequest::WebSocketPush{channel_id, message_type }) => {
+            println!("~\n~\n~\n got request: {:?}, {:?}, {:?}", channel_id, message_type, msg.blob());
+            return Ok(());
+        }
+        Err(e) => {
+            println!("~\n~\n~\n got error: {:?}", e);
+        }
         _ => {}
     }
+
+    Ok(())
+}
+
+
+fn handle_rpc_request(msg: Message, req: RpcRequest, state: &mut State) -> anyhow::Result<()> {
+
+    let inflight = serde_json::to_string(&json!({
+        "jsonrpc": "2.0",
+        "method": req.method,
+        "params": serde_json::from_str::<serde_json::Value>(&req.params.clone()).unwrap(),
+        "id": state.current_id.clone(),
+    })).unwrap();
+
+    state.current_id += 1;
+
+    state.conn.send(Blob {
+        mime: Some("application/json".to_string()),
+        bytes: inflight.into()
+    });
+
+    Ok(())
+}
+
+fn handle_response(our: &Address, msg: Message, state: &mut State) -> anyhow::Result<()> {
 
     Ok(())
 }
