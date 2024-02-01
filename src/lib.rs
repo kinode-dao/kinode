d4b5ce9892c8ea03365c8e6b4a1f450f1461c0f5
hosted-fornet <hosted-fornet@protonmail.com>
2024-01-31 22:46:07 -0800

    try using features to avoid lib importing every dep; WIP

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod eth;
 mod http;
 mod types;
 
-pub use crate::http::client_types as http_client;
-pub use crate::http::server_types as http_server;
-pub use crate::types::*;
 pub use eth::types as eth_types;
+pub use http::client_types as http_client;
+pub use http::server_types as http_server;
+pub use types::*;
