21c24afc3ab026cf64d692cc518b4521920f5832
dr-frmr <docterformer@protonmail.com>
2024-03-08 18:52:41 -0300

    update to wit 0.8.0

--- kinode/build.rs
+++ kinode/build.rs
@@ -12,7 +12,7 @@ fn build_and_zip_package(
 ) -> anyhow::Result<(String, String, Vec<u8>)> {
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
-        kit::build::execute(&entry_path, false, false, false, true).await?;
+        kit::build::execute(&entry_path, false, false, true).await?;
 
         let mut writer = Cursor::new(Vec::new());
         let options = FileOptions::default()
