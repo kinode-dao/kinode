9737154a60a1f34a7fd177d9f2201ac59d0d904f
hosted-fornet <hosted-fornet@protonmail.com>
2024-03-17 11:59:16 -0700

    hotfix: fix a bad merge

--- kinode/packages/app_store/app_store/src/lib.rs
+++ kinode/packages/app_store/app_store/src/lib.rs
@@ -71,6 +71,7 @@ pub enum Resp {
 }
 
 fn fetch_logs(eth_provider: &eth::Provider, filter: &eth::Filter) -> Vec<eth::Log> {
+    #[cfg(not(feature = "simulation-mode"))]
     loop {
         match eth_provider.get_logs(filter) {
             Ok(res) => return res,
@@ -87,6 +88,7 @@ fn fetch_logs(eth_provider: &eth::Provider, filter: &eth::Filter) -> Vec<eth::Lo
 
 #[allow(unused_variables)]
 fn subscribe_to_logs(eth_provider: &eth::Provider, filter: eth::Filter) {
+    #[cfg(not(feature = "simulation-mode"))]
     loop {
         match eth_provider.subscribe(1, filter.clone()) {
             Ok(()) => break,
@@ -97,6 +99,7 @@ fn subscribe_to_logs(eth_provider: &eth::Provider, filter: eth::Filter) {
             }
         }
     }
+    #[cfg(not(feature = "simulation-mode"))]
     println!("subscribed to logs successfully");
 }
 
