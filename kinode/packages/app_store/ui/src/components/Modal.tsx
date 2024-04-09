4f68ecf7108a103201c161b15037c2f6286f12f7
Tobias Merkle <tgamerkle@gmail.com>
2024-04-09 14:31:36 -0400

    modal

--- kinode/packages/app_store/ui/src/components/Modal.tsx
+++ kinode/packages/app_store/ui/src/components/Modal.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import React, { MouseEvent } from 'react'
 import { FaPlus } from 'react-icons/fa'
 
@@ -25,13 +26,30 @@ const Modal: React.FC<ModalProps> = ({
   }
 
   return (
-    <div className={`modal-backdrop ${show ? 'show' : ''}`} onClick={hide}>
-      <div {...props} className={`col modal ${props.className || ''}`} onClick={dontHide}>
-        {Boolean(title) && <h4 className='modal-title'>{title}</h4>}
+    <div
+      className={classNames(`bg-black/25 fixed top-0 bottom-0 left-0 right-0 flex flex-col c z-30 min-h-[10em] min-w-[20em]`,
+        { show }
+      )}
+      onClick={hide}
+    >
+      <div
+        {...props}
+        className={`flex flex-col relative bg-black/90 rounded-lg p-6 ${props.className || ''}`}
+        onClick={dontHide}
+      >
+        {Boolean(title) && <h4 className='mt-0 mb-2'>{title}</h4>}
         {!hideClose && (
-          <FaPlus className='close' onClick={hide} />
+          <button
+            className='icon'
+            onClick={hide}
+          >
+            <FaPlus />
+          </button>
         )}
-        <div className='col modal-content' onClick={dontHide}>
+        <div
+          className='flex flex-col items-center w-full'
+          onClick={dontHide}
+        >
           {props.children}
         </div>
       </div>
