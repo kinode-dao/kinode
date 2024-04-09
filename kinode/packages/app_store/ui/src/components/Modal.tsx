7c05d3568d8785c07e311d85cb1893a237377fb2
Tobias Merkle <tgamerkle@gmail.com>
2024-04-09 16:22:58 -0400

    more modals

--- kinode/packages/app_store/ui/src/components/Modal.tsx
+++ kinode/packages/app_store/ui/src/components/Modal.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import React, { MouseEvent } from 'react'
-import { FaPlus } from 'react-icons/fa'
+import { FaX } from 'react-icons/fa6'
 
 export interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
   show: boolean
@@ -27,23 +27,23 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <div
-      className={classNames(`bg-black/25 fixed top-0 bottom-0 left-0 right-0 flex flex-col c z-30 min-h-[10em] min-w-[20em]`,
+      className={classNames(`bg-black/25 fixed top-0 bottom-0 left-0 right-0 flex flex-col c z-30 min-h-[10em] min-w-[30em]`,
         { show }
       )}
       onClick={hide}
     >
       <div
         {...props}
-        className={`flex flex-col relative bg-black/90 rounded-lg p-6 ${props.className || ''}`}
+        className={`flex flex-col relative bg-black/90 rounded-lg py-6 px-12 ${props.className || ''}`}
         onClick={dontHide}
       >
         {Boolean(title) && <h4 className='mt-0 mb-2'>{title}</h4>}
         {!hideClose && (
           <button
-            className='icon'
+            className='icon absolute top-1 right-1'
             onClick={hide}
           >
-            <FaPlus />
+            <FaX />
           </button>
         )}
         <div
