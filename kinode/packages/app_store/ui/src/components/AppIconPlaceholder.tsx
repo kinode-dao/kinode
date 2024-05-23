dec66b8115a0b9ab66417825f32bbbc1d30d82cb
Tobias Merkle <tgamerkle@gmail.com>
2024-05-23 15:39:02 -0400

    unify icons inside homepage process

--- kinode/packages/app_store/ui/src/components/AppIconPlaceholder.tsx
+++ kinode/packages/app_store/ui/src/components/AppIconPlaceholder.tsx
@@ -1,64 +1,11 @@
 import React from 'react';
 
-// @ts-ignore
-import birdWhite from '../assets/icons/bird-white.svg';
-// @ts-ignore
-import birdOrange from '../assets/icons/bird-orange.svg';
-// @ts-ignore
-import birdPlain from '../assets/icons/bird-plain.svg';
-// @ts-ignore
-import kOrange from '../assets/icons/k-orange.svg';
-// @ts-ignore
-import kPlain from '../assets/icons/k-plain.svg';
-// @ts-ignore
-import kWhite from '../assets/icons/k-white.svg';
-// @ts-ignore
-import kBirdOrange from '../assets/icons/kbird-orange.svg';
-// @ts-ignore
-import kBirdPlain from '../assets/icons/kbird-plain.svg';
-// @ts-ignore
-import kBirdWhite from '../assets/icons/kbird-white.svg';
-// @ts-ignore
-import kBranchOrange from '../assets/icons/kbranch-orange.svg';
-// @ts-ignore
-import kBranchPlain from '../assets/icons/kbranch-plain.svg';
-// @ts-ignore
-import kBranchWhite from '../assets/icons/kbranch-white.svg';
-// @ts-ignore
-import kFlowerOrange from '../assets/icons/kflower-orange.svg';
-// @ts-ignore
-import kFlowerPlain from '../assets/icons/kflower-plain.svg';
-// @ts-ignore
-import kFlowerWhite from '../assets/icons/kflower-white.svg';
-
 import { isMobileCheck } from '../utils/dimensions';
 import classNames from 'classnames';
 
 const AppIconPlaceholder: React.FC<{ text: string, className?: string, size: 'small' | 'medium' | 'large' }> = ({ text, className, size }) => {
-  const index = Number.parseInt(text.split('').pop() || '0', 16)
-  if (!index || isNaN(index)) {
-    return null
-  }
-
-  const images = [
-    birdWhite,
-    birdOrange,
-    birdPlain,
-    kOrange,
-    kPlain,
-    kWhite,
-    kBirdOrange,
-    kBirdPlain,
-    kBirdWhite,
-    kBranchOrange,
-    kBranchPlain,
-    kBranchWhite,
-    kFlowerOrange,
-    kFlowerPlain,
-    kFlowerWhite,
-  ]
-
-  const derivedFilename = images[index % images.length]
+  const index = text.split('').pop()?.toUpperCase() || '0'
+  const derivedFilename = `/icons/${index}`
 
   if (!derivedFilename) {
     return null
--- kinode/packages/homepage/ui/src/components/AppIconPlaceholder.tsx
+++ kinode/packages/homepage/ui/src/components/AppIconPlaceholder.tsx
@@ -1,49 +1,11 @@
 import React from 'react';
 
-import birdWhite from '../assets/icons/bird-white.svg';
-import birdOrange from '../assets/icons/bird-orange.svg';
-import birdPlain from '../assets/icons/bird-plain.svg';
-import kOrange from '../assets/icons/k-orange.svg';
-import kPlain from '../assets/icons/k-plain.svg';
-import kWhite from '../assets/icons/k-white.svg';
-import kBirdOrange from '../assets/icons/kbird-orange.svg';
-import kBirdPlain from '../assets/icons/kbird-plain.svg';
-import kBirdWhite from '../assets/icons/kbird-white.svg';
-import kBranchOrange from '../assets/icons/kbranch-orange.svg';
-import kBranchPlain from '../assets/icons/kbranch-plain.svg';
-import kBranchWhite from '../assets/icons/kbranch-white.svg';
-import kFlowerOrange from '../assets/icons/kflower-orange.svg';
-import kFlowerPlain from '../assets/icons/kflower-plain.svg';
-import kFlowerWhite from '../assets/icons/kflower-white.svg';
-
 import { isMobileCheck } from '../utils/dimensions';
 import classNames from 'classnames';
 
 const AppIconPlaceholder: React.FC<{ text: string, className?: string, size: 'small' | 'medium' | 'large' }> = ({ text, className, size }) => {
-  const index = Number.parseInt(text.split('').pop() || '0', 16)
-  if (!index || isNaN(index)) {
-    return null
-  }
-
-  const images = [
-    birdWhite,
-    birdOrange,
-    birdPlain,
-    kOrange,
-    kPlain,
-    kWhite,
-    kBirdOrange,
-    kBirdPlain,
-    kBirdWhite,
-    kBranchOrange,
-    kBranchPlain,
-    kBranchWhite,
-    kFlowerOrange,
-    kFlowerPlain,
-    kFlowerWhite,
-  ]
-
-  const derivedFilename = images[index % images.length]
+  const index = text.split('').pop()?.toUpperCase() || '0'
+  const derivedFilename = `/icons/${index}`
 
   if (!derivedFilename) {
     return null
