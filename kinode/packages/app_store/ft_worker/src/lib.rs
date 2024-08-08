66b8d8b37a9c99ce7ed2453ff91a3a65e8d9376a
bitful-pannul <bitful-pancreas@protonmail.com>
2024-08-08 14:25:47 +0300

    ft_worker: refactor

--- kinode/packages/app_store/ft_worker/src/lib.rs
+++ kinode/packages/app_store/ft_worker/src/lib.rs
@@ -1,23 +1,26 @@
-use kinode_process_lib::println;
+use crate::kinode::process::downloads::{
+    ChunkRequest, DownloadRequest, Downloads, ProgressUpdate, SizeUpdate,
+};
 use kinode_process_lib::*;
-use serde::{Deserialize, Serialize};
+use kinode_process_lib::{println, vfs::open_file, vfs::File, vfs::SeekFrom};
+use std::str::FromStr;
 
-mod ft_worker_lib;
-use ft_worker_lib::*;
+pub mod ft_worker_lib;
 
 wit_bindgen::generate!({
     path: "target/wit",
-    world: "process-v0",
+    generate_unused_types: true,
+    world: "app-store-sys-v0",
+    additional_derives: [serde::Deserialize, serde::Serialize],
 });
 
-/// internal worker protocol
-#[derive(Debug, Serialize, Deserialize)]
-pub enum FTWorkerProtocol {
-    Ready,
-    Finished,
-}
+const CHUNK_SIZE: u64 = 262144; // 256KB
+
+// TODO: Add a timer request that returns to us whenever the timeout time is back.
+// If we're still alive at that point for any reason, we are getting purged.
 
 call_init!(init);
+
 fn init(our: Address) {
     let Ok(Message::Request {
         source: parent_process,
@@ -28,162 +31,181 @@ fn init(our: Address) {
         panic!("ft_worker: got bad init message");
     };
 
-    let command = serde_json::from_slice::<FTWorkerCommand>(&body)
-        .expect("ft_worker: got unparseable init message");
-
-    let Some(result) = (match command {
-        FTWorkerCommand::Send {
-            target,
-            file_name,
-            timeout,
-        } => Some(handle_send(&our, &target, &file_name, timeout)),
-        FTWorkerCommand::Receive {
-            file_name,
-            total_chunks,
-            timeout,
-            ..
-        } => handle_receive(parent_process, &file_name, total_chunks, timeout),
-    }) else {
-        return;
-    };
-
-    Response::new()
-        .body(serde_json::to_vec(&result).unwrap())
-        .send()
-        .unwrap();
-
-    // job is done
-}
-
-fn handle_send(our: &Address, target: &Address, file_name: &str, timeout: u64) -> FTWorkerResult {
-    let transfer_id: u64 = our.process().parse().unwrap();
-    let Some(blob) = get_blob() else {
-        println!("ft_worker: wasn't given blob!");
-        return FTWorkerResult::Err(TransferError::SourceFailed);
-    };
-    let file_bytes = blob.bytes;
-    let mut file_size = file_bytes.len() as u64;
-    let mut offset: u64 = 0;
-    let chunk_size: u64 = 262144; // 256KB
-    let total_chunks = (file_size as f64 / chunk_size as f64).ceil() as u64;
-    // send a file to another worker
-    // start by telling target to expect a file,
-    // then upon reciving affirmative response,
-    // send contents in chunks and wait for
-    // acknowledgement.
-    let Ok(Ok(response)) = Request::to(target.clone())
-        .body(
-            serde_json::to_vec(&FTWorkerCommand::Receive {
-                transfer_id,
-                file_name: file_name.to_string(),
-                file_size,
-                total_chunks,
-                timeout,
-            })
-            .unwrap(),
-        )
-        .send_and_await_response(timeout)
-    else {
-        return FTWorkerResult::Err(TransferError::TargetOffline);
-    };
-    let opp_worker = response.source();
-    let Ok(FTWorkerProtocol::Ready) = serde_json::from_slice(&response.body()) else {
-        return FTWorkerResult::Err(TransferError::TargetRejected);
-    };
-    // send file in chunks
-    loop {
-        if file_size < chunk_size {
-            // this is the last chunk, so we should expect a Finished response
-            let _ = Request::to(opp_worker.clone())
-                .body(vec![])
-                .blob(LazyLoadBlob {
-                    mime: None,
-                    bytes: file_bytes[offset as usize..offset as usize + file_size as usize]
-                        .to_vec(),
-                })
-                .expects_response(timeout)
-                .send();
-            break;
-        }
-        let _ = Request::to(opp_worker.clone())
-            .body(vec![])
-            .blob(LazyLoadBlob {
-                mime: None,
-                bytes: file_bytes[offset as usize..offset as usize + chunk_size as usize].to_vec(),
-            })
-            .send();
-        file_size -= chunk_size;
-        offset += chunk_size;
+    if parent_process.node() != our.node() {
+        panic!("ft_worker: got bad init message source");
     }
-    // now wait for Finished response
-    let Ok(Message::Response { body, .. }) = await_message() else {
-        return FTWorkerResult::Err(TransferError::TargetRejected);
-    };
-    let Ok(FTWorkerProtocol::Finished) = serde_json::from_slice(&body) else {
-        return FTWorkerResult::Err(TransferError::TargetRejected);
-    };
-    // return success to parent
-    return FTWorkerResult::SendSuccess;
-}
 
-fn handle_receive(
-    parent_process: Address,
-    file_name: &str,
-    total_chunks: u64,
-    timeout: u64,
-) -> Option<FTWorkerResult> {
-    // send Ready response to counterparty
-    Response::new()
-        .body(serde_json::to_vec(&FTWorkerProtocol::Ready).unwrap())
-        .send()
-        .unwrap();
-    // receive a file from a worker, then send it to parent
-    // all messages will be chunks of file. when we receive the
-    // last chunk, send a Finished message to sender and Success to parent.
-    let mut file_bytes = Vec::new();
-    let mut chunks_received = 0;
-    let start_time = std::time::Instant::now();
-    loop {
-        let Ok(Message::Request { .. }) = await_message() else {
-            return Some(FTWorkerResult::Err(TransferError::SourceFailed));
-        };
-        if start_time.elapsed().as_secs() > timeout {
-            return Some(FTWorkerResult::Err(TransferError::SourceFailed));
+    let req: DownloadRequest =
+        serde_json::from_slice(&body).expect("ft_worker: got unparseable init message");
+
+    if let Some(node) = req.download_from {
+        match handle_sender(&node, &req.package_id.into(), &req.desired_version_hash) {
+            Ok(_) => {}
+            Err(e) => println!("send error: {}", e),
         }
-        let Some(blob) = get_blob() else {
-            return Some(FTWorkerResult::Err(TransferError::SourceFailed));
-        };
-        chunks_received += 1;
-        file_bytes.extend(blob.bytes);
-        // send progress update to parent
-        Request::to(parent_process.clone())
-            .body(
-                serde_json::to_vec(&FTWorkerResult::ProgressUpdate {
-                    file_name: file_name.to_string(),
-                    chunks_received,
-                    total_chunks,
-                })
-                .unwrap(),
-            )
-            .send()
-            .unwrap();
-        if chunks_received == total_chunks {
-            break;
+    } else {
+        match handle_receiver(
+            &parent_process,
+            &req.package_id.into(),
+            &req.desired_version_hash,
+        ) {
+            Ok(_) => {}
+            Err(e) => println!("receive error: {}", e),
         }
     }
-    // send Finished message to sender
-    Response::new()
-        .body(serde_json::to_vec(&FTWorkerProtocol::Finished).unwrap())
-        .send()
-        .unwrap();
-    // send Success message to parent
-    Request::to(parent_process)
-        .body(serde_json::to_vec(&FTWorkerResult::ReceiveSuccess(file_name.to_string())).unwrap())
-        .blob(LazyLoadBlob {
-            mime: None,
-            bytes: file_bytes,
-        })
-        .send()
-        .unwrap();
-    None
+}
+
+fn handle_sender(node: &str, package_id: &PackageId, version_hash: &str) -> anyhow::Result<()> {
+    let target_worker = Address::from_str(node)?;
+
+    let filename = format!(
+        "/app_store:sys/downloads/{}:{}-{}.zip",
+        package_id.package_name, package_id.publisher_node, version_hash
+    );
+
+    let mut file = open_file(&filename, false, None)?;
+    let size = file.metadata()?.len;
+    let num_chunks = (size as f64 / CHUNK_SIZE as f64).ceil() as u64;
+
+    Request::new()
+        .body(serde_json::to_vec(&SizeUpdate {
+            package_id: package_id.clone().into(),
+            size,
+        })?)
+        .target(target_worker.clone())
+        .send()?;
+    file.seek(SeekFrom::Start(0))?;
+
+    for i in 0..num_chunks {
+        send_chunk(&mut file, i, size, &target_worker, package_id, version_hash)?;
+    }
+
+    Ok(())
+}
+
+fn handle_receiver(
+    parent_process: &Address,
+    package_id: &PackageId,
+    version_hash: &str,
+) -> anyhow::Result<()> {
+    // TODO: write to a temporary location first, then check hash as we go, then rename to final location.
+    let full_filename = format!(
+        "/app_store:sys/downloads/{}:{}-{}.zip",
+        package_id.package_name, package_id.publisher_node, version_hash
+    );
+
+    let mut file = open_file(&full_filename, true, None)?;
+
+    let mut size: Option<u64> = None;
+
+    loop {
+        let Ok(Message::Request { body, .. }) = await_message() else {
+            return Err(anyhow::anyhow!("ft_worker: got bad message"));
+        };
+
+        let req: Downloads = serde_json::from_slice(&body)?;
+
+        match req {
+            Downloads::Chunk(chunk) => {
+                handle_chunk(&mut file, &chunk, parent_process, &mut size)?;
+                if let Some(s) = size {
+                    if chunk.offset + chunk.length >= s {
+                        return Ok(());
+                    }
+                }
+            }
+            Downloads::Size(update) => {
+                size = Some(update.size);
+            }
+            _ => println!("ft_worker: got unexpected message"),
+        }
+    }
+}
+
+fn send_chunk(
+    file: &mut File,
+    chunk_index: u64,
+    total_size: u64,
+    target: &Address,
+    package_id: &PackageId,
+    version_hash: &str,
+) -> anyhow::Result<()> {
+    let offset = chunk_index * CHUNK_SIZE;
+    let length = CHUNK_SIZE.min(total_size - offset);
+
+    let mut buffer = vec![0; length as usize];
+    // this extra seek might be unnecessary. fix multireads per process in vfs
+    file.seek(SeekFrom::Start(offset))?;
+    file.read_at(&mut buffer)?;
+
+    Request::new()
+        .body(serde_json::to_vec(&ChunkRequest {
+            package_id: package_id.clone().into(),
+            version_hash: version_hash.to_string(),
+            offset,
+            length,
+        })?)
+        .target(target.clone())
+        .blob_bytes(buffer)
+        .send()?;
+    Ok(())
+}
+
+fn handle_chunk(
+    file: &mut File,
+    chunk: &ChunkRequest,
+    parent: &Address,
+    size: &mut Option<u64>,
+) -> anyhow::Result<()> {
+    let bytes = if let Some(blob) = get_blob() {
+        blob.bytes
+    } else {
+        return Err(anyhow::anyhow!("ft_worker: got no blob"));
+    };
+    file.write_all(&bytes)?;
+
+    if let Some(total_size) = size {
+        // let progress = ((chunk.offset + chunk.length) as f64 / *total_size as f64 * 100.0) as u64;
+
+        Request::new()
+            .body(serde_json::to_vec(&ProgressUpdate {
+                package_id: chunk.package_id.clone(),
+                downloaded: chunk.offset + chunk.length,
+                total: *total_size,
+                version_hash: chunk.version_hash.clone(),
+            })?)
+            .target(parent.clone())
+            .send()?;
+    }
+
+    Ok(())
+}
+
+impl crate::kinode::process::main::PackageId {
+    pub fn to_process_lib(&self) -> kinode_process_lib::PackageId {
+        kinode_process_lib::PackageId::new(&self.package_name, &self.publisher_node)
+    }
+
+    pub fn from_process_lib(package_id: &kinode_process_lib::PackageId) -> Self {
+        Self {
+            package_name: package_id.package_name.clone(),
+            publisher_node: package_id.publisher_node.clone(),
+        }
+    }
+}
+// Conversion from wit PackageId to process_lib's PackageId
+impl From<crate::kinode::process::downloads::PackageId> for kinode_process_lib::PackageId {
+    fn from(package_id: crate::kinode::process::downloads::PackageId) -> Self {
+        kinode_process_lib::PackageId::new(&package_id.package_name, &package_id.publisher_node)
+    }
+}
+
+// Conversion from process_lib's PackageId to wit PackageId
+impl From<kinode_process_lib::PackageId> for crate::kinode::process::downloads::PackageId {
+    fn from(package_id: kinode_process_lib::PackageId) -> Self {
+        Self {
+            package_name: package_id.package_name,
+            publisher_node: package_id.publisher_node,
+        }
+    }
 }
