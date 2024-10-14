bbf89ff7130a4ab527e41cedac25069aae38b7a0
hosted-fornet <hosted-fornet@protonmail.com>
2024-10-14 15:49:47 -0700

    lib: eliminate `kit` dependency

--- lib/build.rs
+++ lib/build.rs
@@ -1,8 +1,57 @@
+use std::fs;
+use std::path::Path;
+
+use sha2::Sha256;
+
+const KIT_CACHE: &str = "/tmp/kinode-kit-cache";
 const KINODE_WIT_0_7_0_URL: &str =
     "https://raw.githubusercontent.com/kinode-dao/kinode-wit/aa2c8b11c9171b949d1991c32f58591c0e881f85/kinode.wit";
-
 const KINODE_WIT_0_8_0_URL: &str =
     "https://raw.githubusercontent.com/kinode-dao/kinode-wit/v0.8/kinode.wit";
+
+/// copied from `kit`
+async fn download_file(url: &str, path: &Path) -> anyhow::Result<()> {
+    fs::create_dir_all(&KIT_CACHE)?;
+    let mut hasher = Sha256::new();
+    hasher.update(url.as_bytes());
+    let hashed_url = hasher.finalize();
+    let hashed_url_path = Path::new(KIT_CACHE).join(format!("{hashed_url:x}"));
+
+    let content = if hashed_url_path.exists() {
+        fs::read(hashed_url_path)?
+    } else {
+        let response = reqwest::get(url).await?;
+
+        // Check if response status is 200 (OK)
+        if response.status() != reqwest::StatusCode::OK {
+            return Err(eyre!(
+                "Failed to download file: HTTP Status {}",
+                response.status()
+            ));
+        }
+
+        let content = response.bytes().await?.to_vec();
+        fs::write(hashed_url_path, &content)?;
+        content
+    };
+
+    if path.exists() {
+        if path.is_dir() {
+            fs::remove_dir_all(path)?;
+        } else {
+            let existing_content = fs::read(path)?;
+            if content == existing_content {
+                return Ok(());
+            }
+        }
+    }
+    fs::create_dir_all(
+        path.parent()
+            .ok_or_else(|| eyre!("path doesn't have parent"))?,
+    )?;
+    fs::write(path, &content)?;
+    Ok(())
+}
 
 fn main() {
     if std::env::var("SKIP_BUILD_SCRIPT").is_ok() {
@@ -16,7 +65,7 @@ fn main() {
 
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
-        kit::build::download_file(KINODE_WIT_0_7_0_URL, &wit_file)
+        download_file(KINODE_WIT_0_7_0_URL, &wit_file)
             .await
             .expect("Failed to download WIT 0.7");
     });
@@ -25,7 +74,7 @@ fn main() {
 
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
-        kit::build::download_file(KINODE_WIT_0_8_0_URL, &wit_file)
+        download_file(KINODE_WIT_0_8_0_URL, &wit_file)
             .await
             .expect("Failed to download WIT 0.8");
     })
