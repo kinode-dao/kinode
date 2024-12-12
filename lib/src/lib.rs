6bd249b95fce529c8c623fe3c47ea890a7f61447
hosted-fornet <hosted-fornet@protonmail.com>
2024-12-12 13:45:44 -0800

    bump wasmtime to 0.27 and wit-bindgen to 0.36

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -20,12 +20,22 @@ pub mod types {
 pub use kinode::process;
 pub use kinode::process::standard as wit;
 
+// `trappable_imports: true` keeps behavior the same as pre-240410
+//  where imports are wrapped with an `anyhow::Result`.
+//  This allows errors that occur in imports to be handled naturally,
+//  namely by printing to terminal, e.g.
+//  https://github.com/kinode-dao/kinode/blob/b75cf2c0f9f274edcaa43449b460e6ba11d852a9/kinode/src/kernel/process.rs#L381
+//
+//  source:
+//  https://github.com/bytecodealliance/wasmtime/commit/1cf0060bbc17aaf35b81b989c6394df254bb4f2e
+
 // can remove in 1.0!
 
 wasmtime::component::bindgen!({
     path: "wit-v0.7.0",
     world: "process",
     async: true,
+    trappable_imports: true,
 });
 
 // can remove in 1.0!
@@ -37,6 +47,7 @@ pub mod v0 {
         path: "wit-v0.8.0",
         world: "process-v0",
         async: true,
+        trappable_imports: true,
     });
 }
 
@@ -47,5 +58,6 @@ pub mod v1 {
         path: "wit-v1.0.0",
         world: "process-v1",
         async: true,
+        trappable_imports: true,
     });
 }
